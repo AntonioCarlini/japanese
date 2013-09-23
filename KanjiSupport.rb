8db0a0e5116ade9f6a3e0dd3206f73e019a07c97
Antonio Carlini <arcarlini@iee.org>
2013-09-23 21:51:19 +0100

    Rather than loading kanji.data every time, build a ruby script with a prepared hash derived from kanji.data: the ruby interpreter lodas this much more quickly than parsing the file each time.
    build-kanji-ruby.rb: This script builds the fast-kanji ruby script: it is expected to be run
    manually when kanji.data changes.
    
    FastKanji.rb: This is the automatically generated script.
    
    KanjiSupport.rb: Use the new "fast" kanji support script.
    
    grammar-mada.jhtml:
    grammar-nikui.jhtml: These files turned out to differ after regenerating everything. That turns
    out to be because they used incorrect kanji in the first place. They have been fixed and
    visually inspected.

--- KanjiSupport.rb
+++ KanjiSupport.rb
@@ -3,37 +3,14 @@
 
 $LOAD_PATH << File.dirname(__FILE__)
 
+require 'FastKanji.rb'
 require 'UnicodeSupport.rb'
 
 $kanji_data = nil
 $kanji_by_keyword = {} # hash of keyword (as symbol) to kanji unicode
 
 def find_kanji_unicode_from_keyword(keyword)
-  # read and process the kanji datafile if needed
-  if $kanji_data.nil?()
-    kanji_data_file = "kanji.data" # Hard code this for now
-    $kanji_data = DataKanji.create_from_file(kanji_data_file)
-    $kanji_data.kanji().each() {
-      |k|
-      k.english().each() {
-        |word|
-        idx = word.upcase().gsub(' ', "*")
-        if $kanji_by_keyword[idx].nil?()
-          $kanji_by_keyword[idx] = k
-        end
-      }
-      # The first heisig keyword is used as a lowercase key
-      kwd = k.english().first().downcase().sub(' ', "*")
-      if $kanji_by_keyword[kwd] != nil
-        oh = $kanji_by_keyword[kwd].heisig()
-        $stderr.puts("WARNING: seen [#{kwd}] again; this for [#{k.heisig()}], already stored for [#{oh}]") unless k.heisig() > 2042
-      else
-        $kanji_by_keyword[kwd] = k
-      end
-    }
-  end
-
-  k = $kanji_by_keyword[keyword.downcase()]
+  k = KD.instance().kanji()[keyword]
   return k.nil?() ? nil : k.unicode()
 end
 
