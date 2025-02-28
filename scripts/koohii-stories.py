55d97c114409124781330f02bf7b98d9ade6a862
Antonio Carlini <antonio@acarlini.com>
2025-02-28 20:48:34 +0000

    scripts/koohii-stories.py: Remove the fixed width for the kanji column
    There's no real reason to specify a width for column 3, so remove that for now.

--- scripts/koohii-stories.py
+++ scripts/koohii-stories.py
@@ -237,7 +237,6 @@ def generate_html_table(entries, heisig_keyword_map, frame_number_map, missing_k
             }
             .col1 { width: 6em; }
             .col2 { width: 12em; }
-            .col3 { width: 1em; }
             .col4 { width: 40%; }
             td:nth-child(3) { font-size: 18pt; }
 
