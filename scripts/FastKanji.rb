d40d3d94eb4a45ec79d5855be9060f317320da36
Antonio Carlini <antonio@acarlini.com>
2025-06-19 17:31:53 +0100

    Add the kanji for "stir-fry"
    data/kanji.data: The "stir-fry" kanji isn't one of the daily use ones but it does crop up in 炒める (itameru).
    
    scripts/FastKanji.rb: Rebuild the fast access script

--- scripts/FastKanji.rb
+++ scripts/FastKanji.rb
@@ -3180,6 +3180,7 @@ class KD
     :"go*upstream*(alternate)" => [28335, 28335, ["SO","SAKU"], ["sakanobo.ru"], [""], ["go upstream (alternate)"], 0, 0],
     :"bog" => [28580, 28580, ["TAKU"], ["sawa","uruo.i","uruo.su","tsuya"], [""], ["bog"], 0, 0],
     :"fire*below" => [28780, 28780, ["HYOU","KA"], [""], [""], ["fire below"], 0, 0],
+    :"stir-fry" => [28818, 28818, ["SOU","SHOU"], ["i.ru","ita.meru"], [""], ["stir-fry"], 0, 0],
     :"cauterize" => [28825, 28825, ["SHA","SEKI"], ["abu.ru"], [""], ["cauterize"], 0, 0],
     :"vulture*(old)" => [29227, 29227, [""], [""], [""], ["vulture (old)"], 0, 0],
     :"reptile" => [29228, 29228, ["HA"], ["ka.ku","ha.u"], [""], ["reptile"], 0, 0],
