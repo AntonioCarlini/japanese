c833efbf83ccf0ca0fc123909058e47841f583f1
Antonio Carlini <antonio@acarlini.com>
2025-12-18 22:18:38 +0000

    Add frame #33296, "licking"
    data/kanji.data: Add the kanji ...
    
    scripts/FastKanji.rb: ... and update the fast-access script.

--- scripts/FastKanji.rb
+++ scripts/FastKanji.rb
@@ -3267,6 +3267,7 @@ class KD
     :"small*worm" => [32921, 32921, [""], [""], [""], ["small worm"], 0, 0],
     :"pithole" => [33277, 33277, ["KAN","GEN","KON"], ["otoshiana"], [""], ["pithole"], 0, 0],
     :"magpie" => [33284, 33284, ["SHAKU","SAKU","SEKI","TAKU"], ["kasasagi"], [""], ["magpie"], 0, 0],
+    :"licking" => [33296, 33296, ["SHI"], ["nebu.ru","na.meru"], [""], ["licking"], 0, 0],
     :"dancing*legs" => [33307, 33307, ["BU","SEN"], ["mai","somu.ku"], [""], ["dancing legs"], 0, 0],
     :"bright-silver" => [33390, 33390, ["KON","GON"], ["ushitora"], [""], ["bright-silver"], 0, 0],
     :"grass*radical*full" => [33400, 33400, ["SOU"], ["kusa"], [""], ["grass radical full"], 0, 0],
